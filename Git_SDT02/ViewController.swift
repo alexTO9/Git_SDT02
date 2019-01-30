3fdb7437eb00e0af480b332a7c1e7b692083a314
Alejandro F <lex.inm9@gmail.com>
2019-01-30 18:27:55 -0500

    Initial Commit

---
+++ Git_SDT02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git_SDT02
+//
+//  Created by Alejandro Flores Cruz on 2019-01-30.
+//  Copyright © 2019 Develop Stuff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
